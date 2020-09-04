ceee5cbbb511af778ce1d21d0443cd6d6230c645
kalysbek-z <kalysbek_z@auca.kg>
2020-09-04 11:48:46 +0600

    lab02 p01 Rabotaet

---
+++ .vscode/lab02/p01/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n)
+{
+    int res = 0;
+    while (n != 0)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    while (cin >> n && n != 0)
+    {
+        int s = sumOfDigits(n);
+        for (int i = 11; i <= 100; i++)
+        {
+            if (s == sumOfDigits(i * n))
+            {
+                cout << i << "\n";
+                break;
+            }
+        }
+    }
+}
