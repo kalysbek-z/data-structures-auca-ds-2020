31470966e7b53df537933967b5bf06216c4c71d0
kalysbek-z <kalysbek_z@auca.kg>
2020-09-17 16:21:10 +0600

    lab02 p02 planina accepted

--- lab02/p02/main.cpp
+++ lab02/p02/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int i = (pow(2, n) + 1) * (pow(2, n) + 1);
+    cout << i << "\n";
 }
