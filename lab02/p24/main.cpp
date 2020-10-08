0ee34c630ff78391b1436881d72b7befe72e9138
kalysbek-z <kalysbek_z@auca.kg>
2020-10-08 20:42:17 +0600

    lab02 p24  accepted

--- lab02/p24/main.cpp
+++ lab02/p24/main.cpp
@@ -16,7 +16,7 @@ int main()
 
     for (int i = 2; i <= n / 2 + 1; i++)
     {
-        if (n % (i - 1) == 0 || n % (i - 1) == i)
+        if (n % ( i + i - 1) == 0 || n % (i + i - 1) == i)
         {
             cout << i << "," << i - 1 << "\n";
         }
