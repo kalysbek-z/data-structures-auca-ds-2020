07c6fef5e488118c9948e5eba403e03b88e413f3
kalysbek-z <kalysbek_z@auca.kg>
2020-10-08 20:25:07 +0600

    lab02 p24 wrong

---
+++ lab02/p24/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n << ":\n";
+
+    for (int i = 2; i <= n / 2 + 1; i++)
+    {
+        if (n % (i - 1) == 0 || n % (i - 1) == i)
+        {
+            cout << i << "," << i - 1 << "\n";
+        }
+        if (n % i == 0)
+        {
+            cout << i << "," << i << "\n";
+        }
+    }
+}
