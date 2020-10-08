f67b685462d5b45439d52afd84549d4cb23d8569
kalysbek-z <kalysbek_z@auca.kg>
2020-10-09 04:43:52 +0600

    lab02 p28 accepted

--- lab02/p28/main.cpp
+++ lab02/p28/main.cpp
@@ -7,31 +7,37 @@
 
 using namespace std;
 
-long double price(long double p, long double a, long double b, long double c, long double d, int k)
+double price(double p, double a, double b, double c, double d, int k)
 {
-    long double pr = p * ((long double)sin(a * k + b) + (long double)cos(c * k + d) + 2);
+    double pr = p * (sin(a * k + b) + cos(c * k + d) + 2);
     return pr;
 }
 
 int main()
 {
     int n;
-    long double p, a, b, c, d;
+    double p, a, b, c, d;
     cin >> p >> a >> b >> c >> d >> n;
-    vector<long double> pr;
-    for (int i = 0; i < n; i++)
-    {
-        pr.push_back(price(p, a, b, c, d, i));
-    }
 
-    long double maxim = pr[0];
-    long double res = 0;
-    for (int j = 1; j < n; j++)
+    double maxim = numeric_limits<double>::min();
+    double minim = numeric_limits<double>::min();
+    double res = 0;
+    for (int i = 1; i <= n; i++)
     {
-        res = max(res, maxim - pr[j]);
-        maxim = max(maxim, pr[j]);
+        double pr = price(p, a, b, c, d, i);
+        if (pr > maxim)
+        {
+            maxim = pr;
+            minim = pr;
+        }
+        if (pr < minim || minim < 0)
+        {
+            minim = pr;
+        }
+        if (maxim - minim > res)
+        {
+            res = maxim - minim;
+        }
     }
-
-    cout.precision(9);
-    cout << fixed << res << "\n";
+    printf("%.9f", res);
 }
