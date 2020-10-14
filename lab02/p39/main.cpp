9a23cdacc896724ce2640e645220df60439d3dd5
kalysbek-z <kalysbek_z@auca.kg>
2020-10-14 22:39:46 +0600

    lab02 p39 accepted

--- lab02/p39/main.cpp
+++ lab02/p39/main.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <math.h>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-#include <vector>
 #include <bits/stdc++.h>
-#include <iterator>
 
 using namespace std;
 
@@ -13,37 +6,43 @@ int main()
 {
     int n;
     cin >> n;
-    while (n-- > 0)
+    while (n--)
     {
         int g, m;
         cin >> g >> m;
         vector<int> ag(g);
         vector<int> am(m);
+        int maxG = numeric_limits<int>::min();
+        int maxM = numeric_limits<int>::min();
         for (int i = 0; i < g; i++)
         {
             cin >> ag[i];
+            if (maxG < ag[i])
+            {
+                maxG = ag[i];
+            }
         }
         for (int i = 0; i < m; i++)
         {
             cin >> am[i];
-        }
-        sort(ag.begin(), ag.end(), greater<int>());
-        sort(am.begin(), am.end(), greater<int>());
-        for (int i = 0; i < max(g, m); i++)
-        {
-            if (ag[ag.size() - 1] >= am[am.size() - 1])
+            if (maxM < am[i])
             {
-                am.pop_back();
-            }
-            else if (ag[ag.size() - 1] < am[am.size() - 1])
-            {
-                ag.pop_back();
-            }
-            else
-            {
-                am.pop_back();
+                maxM = am[i];
             }
         }
-        (ag.size() > am.size()) ? cout << "Godzilla\n" : cout << "MechaGodzilla\n";
+        // sort(ag.begin(), ag.end(), greater<int>());
+        // sort(am.begin(), am.end(), greater<int>());
+        // for (int i = 0; i < max(g, m); i++)
+        // {
+        //     if (ag[ag.size() - 1] >= am[am.size() - 1])
+        //     {
+        //         am.pop_back();
+        //     }
+        //     else if (ag[ag.size() - 1] < am[am.size() - 1])
+        //     {
+        //         ag.pop_back();
+        //     }
+        // }
+        (maxG >= maxM) ? cout << "Godzilla\n" : cout << "MechaGodzilla\n";
     }
 }
