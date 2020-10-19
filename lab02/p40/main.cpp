722c6d0c2b34c14533eeb751d15f7ef669d9eca8
kalysbek-z <kalysbek_z@auca.kg>
2020-10-19 12:43:28 +0600

    lab02 p40 wrong ans

---
+++ lab02/p40/main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int r, c;
+    cin >> r >> c;
+    vector<vector<char>> loc(r);
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            char ch;
+            cin >> ch;
+            loc[i].push_back(ch);
+            // cout << loc[i][j] << "\n";
+        }
+    }
+    int row = 0, col = 0, count = 0;
+    while (true)
+    {
+        if (row < 0 || col < 0 || row >= r || col >= c)
+        {
+            cout << "Out./n";
+            break;
+        }
+        if (count > r * c)
+        {
+            cout << "Lost.\n";
+            break;
+        }
+        if (loc[row][col] == 'T')
+        {
+            cout << count << "\n";
+            break;
+        }
+        else if (loc[row][col] == 'N')
+        {
+            row--;
+        }
+        else if (loc[row][col] == 'S')
+        {
+            row++;
+        }
+        else if (loc[row][col] == 'W')
+        {
+            col--;
+        }
+        else if (loc[row][col] == 'E')
+        {
+            col++;
+        }
+        count++;
+    }
+}
