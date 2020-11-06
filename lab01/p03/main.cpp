0b8995fab9659330446c22abf6cd43434763c17c
kalysbek-z <kalysbek_z@auca.kg>
2020-11-06 19:31:26 +0600

    lab01 p3

---
+++ lab01/p03/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void badSwap(int a, int b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
+void goodSwap1(int &a, int &b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
+int main()
+{
+    cout << "1st int: ";
+    int a;
+    cin >> a;
+
+    cout << "2nd int: ";
+    int b;
+    cin >> b;
+
+    cout << "std swap:\n";
+    cout << "Before: a = " << a << ", b = " << b << endl;
+    swap(a, b);
+    cout << "After: a = " << a << ", b = " << b << endl;
+
+    cout << "Bad swap:\n";
+    cout << "Before: a = " << a << ", b = " << b << endl;
+    badSwap(a, b);
+    cout << "After: a = " << a << ", b = " << b << endl;
+
+    cout << "good swap:\n";
+    cout << "Before: a = " << a << ", b = " << b << endl;
+    goodSwap1(a, b);
+    cout << "After: a = " << a << ", b = " << b << endl;
+}
