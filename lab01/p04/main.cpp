4b63dfdfe9c93d32123f13af184d0a933f57a93f
kalysbek-z <kalysbek_z@auca.kg>
2020-11-07 00:46:27 +0600

    lab01 p04

--- lab01/p04/main.cpp
+++ lab01/p04/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <algorithm>
+#include <cctype>
+#include <string>
 
 using namespace std;
 
+bool isPalindrome(const string &line)
+{
+    string b;
+    for (auto e : line)
+    {
+        if (isalpha(e))
+        {
+            b += tolower(e);
+        }
+    }
+    string t = b;
+    reverse(t.begin(), t.end());
+    return b == t;
+}
+
 int main()
 {
+    for (string line; getline(cin, line);)
+    {
+        cout << (isPalindrome(line) ? "palindrome\n" : "not a palindrome\n");
+    }
 }
