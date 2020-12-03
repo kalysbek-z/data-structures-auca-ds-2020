ab4635297793cfd778bec0a1f78b8b81a9d7e7e7
kalysbek-z <kalysbek_z@auca.kg>
2020-12-04 00:46:46 +0600

    uva-11588 accepted

--- project-1/uva-11588/main.cpp
+++ project-1/uva-11588/main.cpp
@@ -7,6 +7,7 @@ struct Freq
     /* data */
     char letter;
     int count;
+
     Freq(const char &l, int c) : letter(l), count(c)
     {
     }
@@ -21,17 +22,52 @@ int main()
         int r, c, m, n;
         cin >> r >> c >> m >> n;
 
-        vector<Freq> freqs(r * c);
+        vector<Freq> freqs;
 
-        for (int j = 0; j < freqs.size(); i++)
+        int size = r * c;
+
+        for (int j = 0; j < size; j++)
         {
             char l;
             cin >> l;
-            freqs.emplace_back(l, 1);
+            freqs.emplace_back(l, 0);
         }
 
-        sort(begin(freqs), end(freqs), [](const Freq &f) {
-
+        sort(begin(freqs), end(freqs), [](Freq &f1, Freq &f2) {
+            return f1.letter < f2.letter;
         });
+
+        int letCount = 0;
+
+        for (int i = 0; i < freqs.size(); i++)
+        {
+            for (int j = 0; j < freqs.size(); j++)
+            {
+                if (freqs[i].letter == freqs[j].letter)
+                {
+                    freqs[i].count++;
+                }
+            }
+        }
+
+        int maxim = 0;
+        vector<char> max_l;
+        int maxCount = 0;
+
+        for (int i = 0; i < freqs.size(); i++)
+        {
+            if (freqs[i].count > maxim)
+            {
+                maxim = freqs[i].count;
+                maxCount = 1;
+                max_l.push_back(freqs[i].letter);
+            }
+            else if (freqs[i].count == maxim)
+            {
+                maxCount++;
+            }
+        }
+
+        cout << "Case " << i + 1 << ": " << maxCount * m + (freqs.size() - maxCount) * n << "\n";
     }
 }
