5f41298d6e1bc239a59a6ae8045a99b4f72a2536
kalysbek-z <kalysbek_z@auca.kg>
2020-12-19 02:53:56 +0600

    lab04 p02

--- lab04/p02/main.c
+++ lab04/p02/main.c
@@ -20,9 +20,27 @@ int main(void)
 
     printf("\n");
 
-    for (int *p = &arr[0]; p != arr + N; p++)
+    for (int *p = arr; p != arr + N; p++)
     {
         printf("%d ", *p);
     }
     printf("\n");
+
+    int sum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        sum += arr[i];
+    }
+
+    printf("sum of all elements: %d\n", sum);
+
+    int sump = 0;
+
+    for (int *p = arr; p != &arr[N]; p++)
+    {
+        sump += *p;
+    }
+
+    printf("sum of all elements using *p: %d\n", sump);
 }
