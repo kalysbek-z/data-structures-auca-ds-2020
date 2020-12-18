967fa38bec779baebdde797f43e7f7c62640bffd
kalysbek-z <kalysbek_z@auca.kg>
2020-12-19 03:08:58 +0600

    lab04 p03

---
+++ lab04/p03/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define N 5
+#define M 5
+
+void printArray(int *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", p[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int a[N];
+    int b[M];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    printArray(a, N);
+    printArray(b, M);
+}
