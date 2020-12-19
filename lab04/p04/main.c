9ef62f30b5d40699e60a4558e6a021bd109639a9
kalysbek-z <kalysbek_z@auca.kg>
2020-12-19 21:09:39 +0600

    lab04 p04

--- lab04/p04/main.c
+++ lab04/p04/main.c
@@ -12,18 +12,48 @@ void printArray(int *p, int n)
     printf("\n");
 }
 
+void reverse(int *beg, int *end)
+{
+    for (;;)
+    {
+        if (beg == end)
+        {
+            break;
+        }
+
+        --end;
+
+        if (beg == end)
+        {
+            break;
+        }
+
+        int t = *beg;
+        *beg = *end;
+        *end = t;
+        beg++;
+    }
+}
+
 int main(void)
 {
     int n = 0;
     scanf("%d", &n);
 
     int *a = (int *)malloc(sizeof(int) * n);
+    if (a == NULL)
+    {
+        printf("not enough dynamic memory");
+    }
 
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
 
+    printArray(a, n);
+
+    reverse(a, a + n);
     printArray(a, n);
 
     free(a);
