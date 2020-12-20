92e5b2bf62a373d01f5126ad337afa1458030f37
kalysbek-z <kalysbek_z@auca.kg>
2020-12-21 00:59:47 +0600

    lab07 p06

---
+++ lab07/p06/main.cpp
@@ -0,0 +1,46 @@
+#include "../../libs/utils.hpp"
+#include <bits/stdc++.h>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+struct Student
+{
+    string mName;
+    double mGpa;
+    Student(const string &name, double gpa)
+        : mName(name), mGpa(gpa)
+    {
+    }
+};
+
+int main()
+{
+    string name;
+    double x;
+    vector<Student> a;
+
+    while (cin >> name >> x)
+    {
+        Student st(name, x);
+        a.push_back(st);
+    }
+
+    cout << "sort by gpa:\n";
+    auSelectSort(a.begin(), a.end(), [](const Student &g1, const Student &g2) { return g1.mGpa < g2.mGpa; });
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i].mGpa << " ";
+    }
+    cout << "\n";
+
+    cout << "sort by name:\n";
+    auSelectSort(a.begin(), a.end(), [](const Student &g1, const Student &g2) { return g1.mName < g2.mName; });
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i].mName << " ";
+    }
+    cout << "\n";
+}
