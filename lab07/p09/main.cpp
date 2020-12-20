098cbc1d68896871b68d144d0a10180ffc1a7cc8
kalysbek-z <kalysbek_z@auca.kg>
2020-12-21 01:00:05 +0600

    lab07 p09

---
+++ lab07/p09/main.cpp
@@ -0,0 +1,25 @@
+#include "../../libs/utils.hpp"
+#include <bits/stdc++.h>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v = {0, 4, 5, 10, 12, 20, 25, 40};
+
+    int n;
+    while (cin >> n)
+    {
+        /* code */
+        if (auBinarySearch(v.begin(), v.end(), n))
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        }
+    }
+}
