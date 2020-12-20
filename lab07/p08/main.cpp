83bfe05beec9b4d46ca85edda3a91be5ba76c921
kalysbek-z <kalysbek_z@auca.kg>
2020-12-21 01:00:01 +0600

    lab07 p08

---
+++ lab07/p08/main.cpp
@@ -0,0 +1,31 @@
+#include "../../libs/utils.hpp"
+#include <bits/stdc++.h>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v = {0, 4, 5, 10, 12, 20, 25, 40};
+
+    int n;
+
+    while (cin >> n)
+    {
+        auto it = auLowerBound(v.begin(), v.end(), n);
+
+        if (*it == n)
+        {
+            cout << "index of first element equal to " << n << " is " << it - v.begin() << "\n";
+        }
+        else if (*it > n)
+        {
+            cout << "index of first element greater than " << n << " is " << it - v.begin() << "\n";
+        }
+        else
+        {
+            cout << "no elements equal or greater than " << n << "\n";
+        }
+    }
+}
