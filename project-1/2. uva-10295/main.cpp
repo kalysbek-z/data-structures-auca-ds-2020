1b0f743874dd23c8d2d5882c41c0ff8641e0da75
kalysbek-z <kalysbek_z@auca.kg>
2020-12-26 17:04:59 +0600

    changed to lower_bound

--- project-1/2. uva-10295/main.cpp
+++ project-1/2. uva-10295/main.cpp
@@ -27,17 +27,19 @@ int main()
         skills.emplace_back(s, num);
     }
 
+    sort(begin(skills), end(skills), [](Skill s1, Skill s2) { return s1.name < s2.name; });
+
     for (int i = 0; i < n; i++)
     {
         int points = 0;
 
         for (string w; cin >> w && w != ".";)
         {
-            auto p = find_if(begin(skills), end(skills), [w](const Skill &s) {
-                return w == s.name;
+            auto p = lower_bound(begin(skills), end(skills), Skill(w, 0), [](const Skill s1, const Skill s2) {
+                return s1.name < s2.name;
             });
 
-            if (p != end(skills))
+            if (p != end(skills) && p->name == w)
             {
                 points += p->money;
             }
