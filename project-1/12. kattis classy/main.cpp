624a5e77fdbe013e915c508f7c87bcb8f3a09df3
kalysbek-z <kalysbek_z@auca.kg>
2020-12-27 18:23:00 +0600

    kattis classy (3)

--- project-1/12. kattis classy/main.cpp
+++ project-1/12. kattis classy/main.cpp
@@ -10,54 +10,45 @@ int main()
     {
         vector<pair<string, string>> names;
         int t;
-        string name, clas;
         cin >> t;
         for (int i = 0; i < t; i++)
         {
-            getline(cin, name, ':');
-            // cout << "name: " << name << "\n";
-            cin.get();
-            getline(cin, clas, ' ');
-            cin.ignore(5, '\n');
-            pair<string, string> p = make_pair(name, clas);
+            string name, clas, x;
+            string rank;
+            cin >> name >> clas >> x;
+            for (int j = 0; j < clas.size(); j++)
+            {
+                if (clas[i] == 'u')
+                {
+                    rank += 'a';
+                }
+                if (clas[i] == 'm')
+                {
+                    rank += 'b';
+                }
+                if (clas[i] == 'o')
+                {
+                    rank += 'c';
+                }
+            }
+            int l = rank.size();
+
+            for (int j = 0; j < l / 2; j++)
+            {
+                swap(rank[j], rank[l - j - 1]);
+            }
+
+            while (l < 10)
+            {
+                l++;
+                rank += 'b';
+            }
+
+            pair<string, string> p = make_pair(name, rank);
             names.push_back(p);
-            // cout << "name " << names[i].first << " h: " << names[i].second << "\n";
         }
 
-        stable_sort(names.begin(), names.end(), [](pair<string, string> p1, pair<string, string> p2) {
-            int h1 = 1, h2 = 1;
-            int pos = 0;
-            string up = "upper";
-            string mid = "middle";
-
-            while ((pos = p1.second.find(up, pos)) != string::npos)
-            {
-                h1 *= 3;
-                pos += up.length();
-            }
-
-            while ((pos = p1.second.find(mid, pos)) != string::npos)
-            {
-                h1 *= 2;
-                pos += up.length();
-            }
-
-            pos = 0;
-
-            while ((pos = p2.second.find(up, pos)) != string::npos)
-            {
-                h1 *= 3;
-                pos += up.length();
-            }
-
-            while ((pos = p2.second.find(mid, pos)) != string::npos)
-            {
-                h2 *= 2;
-                pos += up.length();
-            }
-
-            return h1 > h2;
-        });
+        stable_sort(names.begin(), names.end());
 
         for (int i = 0; i < names.size(); i++)
         {
