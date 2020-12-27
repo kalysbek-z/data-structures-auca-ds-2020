d128d7c81b54a50029b36d9f07339eeb54fd7b6c
kalysbek-z <kalysbek_z@auca.kg>
2020-12-27 23:17:34 +0600

    classy (5) wrong ans

--- project-1/12. kattis classy/main.cpp
+++ project-1/12. kattis classy/main.cpp
@@ -8,7 +8,7 @@ int main()
     cin >> n;
     while (n--)
     {
-        vector<pair<string, int>> names;
+        vector<pair<string, string>> names;
         int t;
         cin >> t;
         for (int i = 0; i < t; i++)
@@ -17,30 +17,63 @@ int main()
             int rank = 0;
             cin >> name >> clas >> x;
 
-            for (int j = 0; j < clas.size(); j++)
-            {
-                if (clas[j] == 'u')
-                {
-                    rank += 1;
-                }
-                if (clas[j] == 'm')
-                {
-                    rank += 0;
-                }
-                if (clas[j] == 'o')
-                {
-                    rank += -1;
-                }
-            }
+            // for (int j = 0; j < clas.size(); j++)
+            // {
+            //     if (clas[j] == 'u')
+            //     {
+            //         rank += 1;
+            //     }
+            //     if (clas[j] == 'm')
+            //     {
+            //         rank += 0;
+            //     }
+            //     if (clas[j] == 'o')
+            //     {
+            //         rank += -1;
+            //     }
+            // }
 
             // cout << rank << "\n";
-            pair<string, int> p = make_pair(name, rank);
+            pair<string, string> p = make_pair(name, clas);
             names.push_back(p);
             names[i].first.pop_back();
         }
 
-        stable_sort(names.begin(), names.end(), [](pair<string, int> p1, pair<string, int> p2) {
-            return p1.second > p2.second;
+        sort(names.begin(), names.end(), [](pair<string, string> p1, pair<string, string> p2) {
+            int rank1 = 0;
+            for (int j = 0; j < p1.second.size(); j++)
+            {
+                if (p1.second[j] == 'u')
+                {
+                    rank1 += 1;
+                }
+                if (p1.second[j] == 'm')
+                {
+                    rank1 += 0;
+                }
+                if (p1.second[j] == 'o')
+                {
+                    rank1 += -1;
+                }
+            }
+
+            int rank2 = 0;
+            for (int j = 0; j < p2.second.size(); j++)
+            {
+                if (p2.second[j] == 'u')
+                {
+                    rank2 += 1;
+                }
+                if (p2.second[j] == 'm')
+                {
+                    rank2 += 0;
+                }
+                if (p2.second[j] == 'o')
+                {
+                    rank2 += -1;
+                }
+            }
+            return rank1 > rank2;
         });
 
         for (int i = 0; i < names.size(); i++)
