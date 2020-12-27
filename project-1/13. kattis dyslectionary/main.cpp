57c7d8d79b853d979fabcf5ebc6c1bd807e49b56
kalysbek-z <kalysbek_z@auca.kg>
2020-12-27 19:19:10 +0600

    dyslectionary (1)

---
+++ project-1/13. kattis dyslectionary/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void make(vector<string> &v)
+{
+    int m = 0;
+    for (auto i : v)
+    {
+        if (i.size() > m)
+        {
+            m = i.size();
+        }
+    }
+
+    sort(v.begin(), v.end(), [](string s1, string s2) {
+        reverse(s1.begin(), s2.end());
+        reverse(s2.begin(), s2.end());
+        return s1 < s2;
+    });
+
+    for (auto &i : v)
+    {
+        int spaces = m - i.size();
+
+        for (int i = 0; i < spaces; i++)
+        {
+            cout << " ";
+        }
+        cout << i << "\n";
+    }
+}
+
+int main()
+{
+    string s;
+    vector<string> v;
+    while (getline(cin, s))
+    {
+        if (s == "")
+        {
+            make(v);
+            cout << "\n";
+            v.clear();
+            continue;
+        }
+
+        v.push_back(s);
+    }
+    make(v);
+}
